75f0b8b11acfa2cfd1406d87d5ddb2f7ae4af5d0
Chris Kolkman <ckolkman@treorisoft.com>
2022-02-08 15:50:11 -0500

    update to dart-sass (namely for a related dependabot alert)

--- scripts/build-scss.js
+++ scripts/build-scss.js
@@ -1,13 +1,15 @@
 const path = require('path');
 const fs = require('fs');
-const sass = require('node-sass');
+const url = require('url');
+const sass = require('sass');
 const mkdirp = require('mkdirp');
-const tildeImporter = require('node-sass-tilde-importer');
 
-const distFolder = path.join(__dirname, '..', 'dist');
+const rootFolder = path.join(__dirname, '..');
+const distFolder = path.join(rootFolder, 'dist');
 const rawFile = 'src/library/index.scss';
-const actualFile = path.join(__dirname, '..', rawFile);
-const outFile = path.join(__dirname, '..', 'dist/style.css');
+const actualFile = path.join(rootFolder, rawFile);
+const outFile = path.join(rootFolder, 'dist/style.css');
+const nodeModulesFolder = path.join(rootFolder, 'node_modules');
 
 if (!fs.existsSync(actualFile)) {
   console.log('No css to build');
@@ -16,19 +18,27 @@ if (!fs.existsSync(actualFile)) {
 
 console.log('Building css');
 mkdirp.sync(distFolder);
-sass.render({
-  file: actualFile,
-  outFile,
-  outputStyle: 'nested',
+const sassResult = sass.compile(actualFile, {
+  style: 'expanded',
   sourceMap: true,
-  importer: [tildeImporter],
-}, (err, result) => {
-  if (err) {
-    console.error(err.message);
-    process.exit(1);
-  }
-  fs.writeFileSync(outFile, result.css);
-  fs.writeFileSync(outFile + '.map', result.map);
-  console.log('  ...completed');
-  process.exit(0);
+  loadPaths: [
+    nodeModulesFolder,
+  ]
 });
+
+if (sassResult.sourceMap) {
+  const smComment = `/*# sourceMappingURL=style.css.map */`
+  fs.writeFileSync(outFile, sassResult.css + "\n\n" + smComment);
+  sassResult.sourceMap.sources = sassResult.sourceMap.sources.map(filePath => {
+    let parsed = url.fileURLToPath(filePath);
+    return path.relative(distFolder, parsed)
+      .split(path.sep).join(path.posix.sep);
+  });
+  sassResult.sourceMap.file = 'style.css';
+  fs.writeFileSync(outFile + '.map', JSON.stringify(sassResult.sourceMap));
+} else {
+  fs.writeFileSync(outFile, sassResult.css);
+}
+
+console.log('  ...completed');
+process.exit(0);
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = {
             loader: 'sass-loader',
             options: {
               sourceMap: true,
+              implementation: require('sass'),
             }
           }
         ]
